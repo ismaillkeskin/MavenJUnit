9951879b2a9da9a6a6d6b37b735951ab11f507ad
İsmail <keskin9756@hotmail.com>
2023-07-07 23:39:28 +0300

    07/07/2023

---
+++ src/test/java/techproed/day23_Log4j/C01_Log4j.java
@@ -0,0 +1,33 @@
+package techproed.day23_Log4j;
+
+import org.apache.logging.log4j.LogManager;
+import org.junit.Test;
+
+import org.apache.logging.log4j.Logger;
+
+
+
+public class C01_Log4j {
+
+    @Test
+    public  void log4j() {
+      /*
+      Log4j system.out.println gibi her test adiminda bilgilendirme yapmamiza yarayan bir api' dir
+            Log4j ile bu bilgilendirmeleri hem konsolda tarih saat olarak hem de src>main>resources icine
+        koydugumuz log4j.xml file da belirttigimiz .log uzantili dosyada yine tarih saat olarak kayit altina
+        almis olacagiz.
+            Log4j kullanabilmek icin oncelikle log4j-api ve log4j-core dependency lerini mvnrepository.com
+        adresinden (ayni version numaralarina sahip ) alip pom.xml dosyamiza koymaliyiz.
+            src>main>resources icine koydugumuz log4j.xml dosyasindaki kodlara pdf deki adresten ulasabilirsiniz
+       */
+     Logger logger = LogManager.getLogger(C01_Log4j.class);
+     logger.info("INFO");
+     logger.debug("DEBUG");
+     logger.error("ERROR");
+     logger.fatal("FATAL");
+     logger.warn("WARNING");
+
+
+
+    }
+}
